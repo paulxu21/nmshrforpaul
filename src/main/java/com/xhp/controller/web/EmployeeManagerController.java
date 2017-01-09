f1390f1ed797bd9ec233caf1b5ac5139590abca4
paulxu21 <paul_xu21@hotmail.com>
2017-01-10 01:02:22 +0800

    20170110 1:01

--- src/main/java/com/xhp/controller/web/EmployeeManagerController.java
+++ src/main/java/com/xhp/controller/web/EmployeeManagerController.java
@@ -1,11 +1,31 @@
 package com.xhp.controller.web;
 
+import com.xhp.dao.EmployeeDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 /**
  * Created by xuhaipeng on 2017/1/9.
  */
 @Controller
 public class EmployeeManagerController {
+
+    @Autowired
+    private  EmployeeDao employeeDao;
+
+    @GetMapping(value = "/hello")
+    public String sayHello(Map<String, Object> map){
+        map.put("hello", "from TemplateController.helloHtml");
+        return "hello";
+    }
+
+    @GetMapping(value = "/listEmployee")
+    public String listEmployee(Map<String, Object> map){
+        map.put("employees", employeeDao.findAll());
+        return "listemployee";
+    }
 
 }
